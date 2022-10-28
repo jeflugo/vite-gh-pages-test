1aee5b1c0eaf1a81e0e8e529beeaf21bfbda6d7d
Jeferson Lugo <jefersoneduardoluego@gmail.com>
2022-10-28 16:35:56 -0400

    first commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:jeflugo/vite-gh-pages-test.git main:gh-pages
+
+cd -
